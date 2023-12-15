67ebd86c140fb33d24c03e95c03c7bfb5ba5478c
Heidi Vasquez <hlvasque@usc.edu>
2023-12-14 19:04:48 -0800

    added home page

---
+++ vsCodeHomePageTestScript.js
@@ -0,0 +1,38 @@
+function redClick(){
+    document.body.style.backgroundColor = '#F778A1';  
+    resetStyleBoxes(); 
+    changeColorText( 'color: #F778A1;' ); 
+}
+
+function blueClick(){
+    document.body.style.backgroundColor = '#6698FF'; 
+    resetStyleBoxes(); 
+    changeColorText( 'color: #6698FF;' ); 
+}
+
+function yellowClick(){
+    document.body.style.backgroundColor = '#FFE87C'; 
+    resetStyleBoxes(); 
+    changeColorText( 'color: #FFE87C;' ); 
+}
+
+function greenClick(){
+    document.body.style.backgroundColor = '#34A56F'; 
+    resetStyleBoxes(); 
+    changeColorText( 'color: #34A56F;' ); 
+}
+
+function whiteClick(){
+    document.body.style.backgroundColor = 'white'; 
+    changeColorText( 'color: #FFFFFF;' ); 
+}
+
+function resetStyleBoxes(){
+    document.getElementById( "styleBox1" ).style.backgroundColor = 'white'; 
+    document.getElementById( "styleBox2" ).style.backgroundColor = 'white'; 
+    document.getElementById( "styleBox3" ).style.backgroundColor = 'white';
+}
+
+function changeColorText( newcolorString ){
+    document.getElementById( "chosenColor" ).innerText = newcolorString; 
+}
